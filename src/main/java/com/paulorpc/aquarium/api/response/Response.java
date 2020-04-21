bb35a54928de1f2d606a9cef008837d1da722639
paulorpc <paulorpc@gmail.com>
2020-04-21 20:38:01 -0300

    Added classe response para padronizar resposta dos serviços.

---
+++ src/main/java/com/paulorpc/aquarium/api/response/Response.java
@@ -0,0 +1,45 @@
+package com.paulorpc.aquarium.api.response;
+
+import java.util.ArrayList;
+
+import org.springframework.validation.BindingResult;
+
+
+public class Response<T> {
+	
+	private T data;
+	private ArrayList<String> issues;
+	
+	
+	public Response() {
+		issues = new ArrayList<>();
+	}
+	
+	
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public ArrayList<String> getErrors() {
+		return issues;
+	}
+
+	public void setIssues(ArrayList<String> errors) {
+		this.issues = errors;
+	}
+
+	/***
+	 * Adiciona os erros do result para a lista do response.
+	 * @param result resulado da validação
+	 */
+	public void setIssuesFromResultErrors(BindingResult result) {
+		result.getAllErrors().forEach(e->getErrors().add(e.getDefaultMessage()));
+	}
+	
+	
+}
