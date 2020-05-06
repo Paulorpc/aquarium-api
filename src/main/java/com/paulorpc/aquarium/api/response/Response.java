5d2b7b82abdcee9d55af39264aea8cc076f83028
paulorpc <paulorpc@gmail.com>
2020-05-05 21:04:13 -0300

    Aplicado code style formatter do google

--- src/main/java/com/paulorpc/aquarium/api/response/Response.java
+++ src/main/java/com/paulorpc/aquarium/api/response/Response.java
@@ -5,53 +5,65 @@ import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.springframework.validation.BindingResult;
 
-
 public class Response<T> {
-	
-	private T data;
-	private ArrayList<String> issues;
-	
-	
-	public Response() {
-		issues = new ArrayList<>();
-	}
-	
-	
 
-	public T getData() {
-		return data;
-	}
+  private T data;
+  private ArrayList<String> issues;
 
-	public void setData(T data) {
-		this.data = data;
-	}
+  public Response() {
+    issues = new ArrayList<>();
+  }
 
-	public ArrayList<String> getIssues() {
-		return issues;
-	}
+  public T getData() {
+    return data;
+  }
 
-	public void setIssues(ArrayList<String> errors) {
-		this.issues = errors;
-	}
-	
-	
-	/***
-	 * Adiciona os erros da validação para a lista de informações (issues) retornadas ao client. Esse método irá fazer o logging do tipo WARN.
-	 * @param result resulado da validação
-	 * @param Logger Instancia para log de erros
-	 */	
-	public void setIssuesFromResultErrors(BindingResult result, Logger log) {
-		result.getAllErrors().forEach(e->getIssues().add(e.getDefaultMessage()));
-		log.warn("Requisição continha erros: " +getIssues().toString());
-	}
+  public void setData(T data) {
+    this.data = data;
+  }
 
-	
-	/***
-	 * Adiciona os erros da validação para a lista de informações (issues) retornadas ao client. 
-	 * @param result resulado da validação
-	 */
-	public void setIssuesFromResultErrors(BindingResult result) {
-		result.getAllErrors().forEach(e->getIssues().add(e.getDefaultMessage()));
-	}
-	
+  public ArrayList<String> getIssues() {
+    return issues;
+  }
+
+  public void setIssues(ArrayList<String> errors) {
+    issues = errors;
+  }
+
+  /***
+   * Adiciona uma informação a lista de informações (issues).
+   * 
+   * @param issue
+   */
+  public boolean addIssue(String issue, Logger log) {
+    try {
+      log.warn("Requisição continha erros: {}", issue);
+      getIssues().add(issue);
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+
+  /***
+   * Adiciona os erros da validação para a lista de informações (issues) retornadas ao client. Esse
+   * método irá fazer o logging do tipo WARN.
+   * 
+   * @param result resulado da validação
+   * @param Logger Instancia para log de erros
+   */
+  public void setIssuesFromResultErrors(BindingResult result, Logger log) {
+    result.getAllErrors().forEach(e -> getIssues().add(e.getDefaultMessage()));
+    log.warn("Requisição continha erros: " + getIssues().toString());
+  }
+
+  /***
+   * Adiciona os erros da validação para a lista de informações (issues) retornadas ao client.
+   * 
+   * @param result resulado da validação
+   */
+  public void setIssuesFromResultErrors(BindingResult result) {
+    result.getAllErrors().forEach(e -> getIssues().add(e.getDefaultMessage()));
+  }
+
 }
