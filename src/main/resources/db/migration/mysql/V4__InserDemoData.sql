7aa9c2633e7b3298899fe32b7ebe15176576b6b7
paulorpc <paulorpc@gmail.com>
2020-05-17 13:54:44 -0300

    Add update para vincular taxonomia a biota

--- src/main/resources/db/migration/mysql/V4__InserDemoData.sql
+++ src/main/resources/db/migration/mysql/V4__InserDemoData.sql
@@ -1,3 +1,6 @@
 -- TAXONOMIA
 INSERT INTO BiotaTaxonomia(dominio, reino, filo, classe, ordem, familia, genero, especie) VALUES
 ('Animalia', 'Animalia', null, null, 'Acanthuriformes', 'Acanthuridae', 'Paracanthurus', 'hepatus');
+
+-- VINCULA BIOTA A SUA TAXONOMIA
+UPDATE Biota SET idTaxonomia = 1 WHERE idBiota = 2;
